87dba3fcb3562100b829e75b1989feef0142079d
codeplay007 <bajajvibhu@hotmail.com>
2023-04-15 19:22:25 +0530

    CTA, Footer, Hamburger Button And Mobile Menu Done.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
+
+btn.addEventListener('click', navToggle);
 
 linkForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -31,3 +35,9 @@ function validURL(str) {
     )
     return !!pattern.test(str)
   }
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+}
